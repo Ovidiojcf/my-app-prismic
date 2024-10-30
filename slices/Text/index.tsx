30cbcbf6e9cbd028e755e47b33d6040b3021f368
Ovidiojcf <ojcfarias@gmail.com>
2024-10-30 16:17:53 -0300

    tailwind css slice text gradient

---
+++ slices/Text/index.tsx
@@ -0,0 +1,64 @@
+import { Content } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Text`.
+ */
+export type TextProps = SliceComponentProps<Content.TextSlice>;
+
+/**
+ * Component for "Text" Slices.
+ */
+const Text = ({ slice }: TextProps): JSX.Element => {
+  return (
+    <section className="bg-white"
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <header className="absolute inset-x-0 top-0 z-30">
+          <div className="text-center">
+            <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+              <PrismicRichText field={slice.primary.heading} />
+            </h1>
+            <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+              <PrismicRichText field={slice.primary.paragraph} />
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              <PrismicNextLink field={slice.primary.request_a_demo} />
+            </a>
+            </div>
+          </div>
+      </header>
+
+      <div
+        aria-hidden="true"
+        className="transform-gpu overflow-hidden blur-3xl"
+      >
+        <div
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+          className="w-full h-48 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+        />
+      </div>
+
+      <div
+        aria-hidden="true"
+        className="transform-gpu overflow-hidden blur-3xl mt-10"
+      >
+        <div
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+          className="w-full h-48 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Text;
